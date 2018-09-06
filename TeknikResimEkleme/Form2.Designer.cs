fa41496b6dff2cd8898c5a9a72484450189d49fd
debruw <ebru_cakar_11@hotmail.com>
2018-09-06 11:17:38 +0300

    First init

---
+++ TeknikResimEkleme/Form2.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace TeknikResimEkleme
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lookUpEdit3 = new DevExpress.XtraEditors.LookUpEdit();
+            this.lookUpEdit2 = new DevExpress.XtraEditors.LookUpEdit();
+            this.lookUpEdit1 = new DevExpress.XtraEditors.LookUpEdit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Listele = new System.Windows.Forms.Button();
+            this.lbl_StokKod = new System.Windows.Forms.Label();
+            this.lbl_Gk5 = new System.Windows.Forms.Label();
+            this.btn_temizle = new System.Windows.Forms.Button();
+            this.btn_Kayıt = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit3.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lookUpEdit3);
+            this.groupBox1.Controls.Add(this.lookUpEdit2);
+            this.groupBox1.Controls.Add(this.lookUpEdit1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btn_Listele);
+            this.groupBox1.Controls.Add(this.lbl_StokKod);
+            this.groupBox1.Controls.Add(this.lbl_Gk5);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(498, 93);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
+            // lookUpEdit3
+            // 
+            this.lookUpEdit3.Location = new System.Drawing.Point(78, 56);
+            this.lookUpEdit3.Name = "lookUpEdit3";
+            this.lookUpEdit3.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.lookUpEdit3.Properties.NullText = "[Grup Kodu Seçiniz.]";
+            this.lookUpEdit3.Size = new System.Drawing.Size(121, 20);
+            this.lookUpEdit3.TabIndex = 14;
+            // 
+            // lookUpEdit2
+            // 
+            this.lookUpEdit2.Location = new System.Drawing.Point(235, 24);
+            this.lookUpEdit2.Name = "lookUpEdit2";
+            this.lookUpEdit2.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.lookUpEdit2.Properties.NullText = "[Stok Kartı Seçiniz.]";
+            this.lookUpEdit2.Size = new System.Drawing.Size(121, 20);
+            this.lookUpEdit2.TabIndex = 13;
+            // 
+            // lookUpEdit1
+            // 
+            this.lookUpEdit1.Location = new System.Drawing.Point(78, 24);
+            this.lookUpEdit1.Name = "lookUpEdit1";
+            this.lookUpEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.lookUpEdit1.Properties.NullText = "[Stok Kartı Seçiniz.]";
+            this.lookUpEdit1.Size = new System.Drawing.Size(121, 20);
+            this.lookUpEdit1.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(205, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = ">>";
+            // 
+            // btn_Listele
+            // 
+            this.btn_Listele.Location = new System.Drawing.Point(388, 47);
+            this.btn_Listele.Name = "btn_Listele";
+            this.btn_Listele.Size = new System.Drawing.Size(90, 29);
+            this.btn_Listele.TabIndex = 4;
+            this.btn_Listele.Text = "Listele";
+            this.btn_Listele.UseVisualStyleBackColor = true;
+            this.btn_Listele.Click += new System.EventHandler(this.btn_Listele_Click);
+            // 
+            // lbl_StokKod
+            // 
+            this.lbl_StokKod.AutoSize = true;
+            this.lbl_StokKod.ForeColor = System.Drawing.Color.Black;
+            this.lbl_StokKod.Location = new System.Drawing.Point(6, 27);
+            this.lbl_StokKod.Name = "lbl_StokKod";
+            this.lbl_StokKod.Size = new System.Drawing.Size(66, 13);
+            this.lbl_StokKod.TabIndex = 8;
+            this.lbl_StokKod.Text = "Stok Kodu : ";
+            // 
+            // lbl_Gk5
+            // 
+            this.lbl_Gk5.AutoSize = true;
+            this.lbl_Gk5.ForeColor = System.Drawing.Color.Black;
+            this.lbl_Gk5.Location = new System.Drawing.Point(6, 59);
+            this.lbl_Gk5.Name = "lbl_Gk5";
+            this.lbl_Gk5.Size = new System.Drawing.Size(67, 13);
+            this.lbl_Gk5.TabIndex = 9;
+            this.lbl_Gk5.Text = "Grup Kodu : ";
+            // 
+            // btn_temizle
+            // 
+            this.btn_temizle.Location = new System.Drawing.Point(544, 25);
+            this.btn_temizle.Name = "btn_temizle";
+            this.btn_temizle.Size = new System.Drawing.Size(91, 40);
+            this.btn_temizle.TabIndex = 15;
+            this.btn_temizle.Text = "Temizle";
+            this.btn_temizle.UseVisualStyleBackColor = true;
+            this.btn_temizle.Click += new System.EventHandler(this.btn_temizle_Click);
+            // 
+            // btn_Kayıt
+            // 
+            this.btn_Kayıt.Location = new System.Drawing.Point(655, 25);
+            this.btn_Kayıt.Name = "btn_Kayıt";
+            this.btn_Kayıt.Size = new System.Drawing.Size(91, 40);
+            this.btn_Kayıt.TabIndex = 12;
+            this.btn_Kayıt.Text = "Kaydet";
+            this.btn_Kayıt.UseVisualStyleBackColor = true;
+            this.btn_Kayıt.Click += new System.EventHandler(this.btn_Kayıt_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 112);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(936, 467);
+            this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 591);
+            this.Controls.Add(this.btn_temizle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_Kayıt);
+            this.HelpButton = true;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Teknik Resim Ekleme";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit3.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private DevExpress.XtraEditors.LookUpEdit lookUpEdit3;
+        private DevExpress.XtraEditors.LookUpEdit lookUpEdit2;
+        private DevExpress.XtraEditors.LookUpEdit lookUpEdit1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Listele;
+        private System.Windows.Forms.Label lbl_StokKod;
+        private System.Windows.Forms.Label lbl_Gk5;
+        private System.Windows.Forms.Button btn_Kayıt;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_temizle;
+    }
+}
---
+++ TeknikResimEkleme/Form2.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Text;
+
+
+namespace TeknikResimEkleme
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data Source=WIN-OMQO47A64BF\\AYREF;Initial Catalog=HORTUM_2016;Integrated Security=True");
+        SqlDataAdapter da;
+        DataSet ds1;
+
+        private void btn_Listele_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView1.Columns.Clear();
+                string sorgu = "SELECT LTRIM(RTRIM(STK.KOD)) AS 'STOK KODU',LTRIM(RTRIM(STK.AD)) AS 'STOK ADI' FROM STOK00 AS STK(NOLOCK) where  NOT exists(SELECT DISTINCT STKP.EVRAKNO FROM STOK0P AS STKP WHERE STKP.EVRAKNO = STK.KOD) ";
+                string sart1 = " AND STK.KOD >= '" + lookUpEdit1.Text + "'";
+                string sart2 = " AND STK.KOD <= '" + lookUpEdit2.Text + "'";
+                string sart3 = " AND STK.GK_5 = '" + lookUpEdit3.Text + "'";
+                if (lookUpEdit1.Text != "[Stok Kartı Seçiniz.]")
+                {
+                    sorgu = sorgu + sart1;
+                }
+                if (lookUpEdit2.Text != "[Stok Kartı Seçiniz.]")
+                {
+                    sorgu = sorgu + sart2;
+                }
+                if (lookUpEdit3.Text != "[Grup Kodu Seçiniz.]")
+                {
+                    sorgu = sorgu + sart3;
+                }
+                sorgu = sorgu + "ORDER BY STK.KOD";
+                con.Open();
+                da = new SqlDataAdapter(sorgu, con);
+                ds1 = new DataSet();
+                da.Fill(ds1, "STOK00");
+                dataGridView1.DataSource = ds1.Tables[0];
+                con.Close();
+             
+
+                DataGridViewTextBoxColumn gridtxtbx1 = new DataGridViewTextBoxColumn();
+                dataGridView1.Columns.Add(gridtxtbx1);
+                gridtxtbx1.HeaderText = "Teknik Resim 1";
+                gridtxtbx1.Name = "gridtxtbx1";
+
+
+                DataGridViewTextBoxColumn gridtxtbx2 = new DataGridViewTextBoxColumn();
+                dataGridView1.Columns.Add(gridtxtbx2);
+                gridtxtbx2.HeaderText = "Teknik Resim 2";
+                gridtxtbx2.Name = "gridtxtbx2";
+
+
+                DataGridViewTextBoxColumn gridtxtbx3 = new DataGridViewTextBoxColumn();
+                dataGridView1.Columns.Add(gridtxtbx3);
+                gridtxtbx3.HeaderText = "Teknik Resim 3";
+                gridtxtbx3.Name = "gridtxtbx3";
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                da = new SqlDataAdapter("SELECT STK.KOD FROM STOK00 AS STK  WHERE NOT exists (SELECT DISTINCT STKP.EVRAKNO FROM STOK0P AS STKP WHERE STKP.EVRAKNO = STK.KOD ) ORDER BY STK.KOD", con);
+                ds1 = new DataSet();
+                da.Fill(ds1, "STOK00");
+                lookUpEdit1.Properties.DataSource = ds1.Tables[0];
+                lookUpEdit2.Properties.DataSource = ds1.Tables[0];
+
+                lookUpEdit1.Properties.DisplayMember = "KOD";
+                lookUpEdit1.Properties.ValueMember = "KOD";
+
+                lookUpEdit2.Properties.DisplayMember = "KOD";
+                lookUpEdit2.Properties.ValueMember = "KOD";
+
+                da = new SqlDataAdapter("SELECT KOD FROM GECOUST WHERE EVRAKNO='STKGK5'", con);
+                ds1 = new DataSet();
+                da.Fill(ds1, "GECOUST");
+                lookUpEdit3.Properties.DataSource = ds1.Tables[0];
+                lookUpEdit3.Properties.DisplayMember = "KOD";
+                lookUpEdit3.Properties.ValueMember = "KOD";
+                con.Close();
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+
+        private void btn_temizle_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 2) //butonun bulunduğu sütun sırası. Griddeki diğer sütunlar üzerinde yapılan tıklamaları dikkate almasın
+            {
+                OpenFileDialog dosya = new OpenFileDialog();
+                dosya.Filter = "Resim Dosyası |*.jpg;*.nef;*.png| Tüm Dosyalar |*.*";
+                dosya.Title = "Dosya gezgini";
+                dosya.ShowDialog();
+                object DosyaYolu = dosya.FileName;
+                dataGridView1.Rows[e.RowIndex].Cells["gridtxtbx1"].Value =  dosya.FileName;
+            }
+            if (e.ColumnIndex == 3) //butonun bulunduğu sütun sırası. Griddeki diğer sütunlar üzerinde yapılan tıklamaları dikkate almasın
+            {
+                OpenFileDialog dosya = new OpenFileDialog();
+                dosya.Filter = "Resim Dosyası |*.jpg;*.nef;*.png| Tüm Dosyalar |*.*";
+                dosya.Title = "Dosya gezgini";
+                dosya.ShowDialog();
+                object DosyaYolu = dosya.FileName;
+                dataGridView1.Rows[e.RowIndex].Cells["gridtxtbx2"].Value = dosya.FileName;
+            }
+            if (e.ColumnIndex == 4) //butonun bulunduğu sütun sırası. Griddeki diğer sütunlar üzerinde yapılan tıklamaları dikkate almasın
+            {
+                OpenFileDialog dosya = new OpenFileDialog();
+                dosya.Filter = "Resim Dosyası |*.jpg;*.nef;*.png| Tüm Dosyalar |*.*";
+                dosya.Title = "Dosya gezgini";
+                dosya.ShowDialog();
+                object DosyaYolu = dosya.FileName;
+                dataGridView1.Rows[e.RowIndex].Cells["gridtxtbx3"].Value = dosya.FileName;
+            }
+        }
+
+        private void btn_Kayıt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Type dinamoType = Type.GetTypeFromProgID("D7ebcom1.DocCreator.1");
+                dynamic dinamoInstance = Activator.CreateInstance(dinamoType);
+                dinamoInstance.SetWorkDirectory(@"C:\d7s\_HORTUM_2017");
+                var doc_STOK00 = dinamoInstance.CreateDocumentU("STOK00", "D7KSUPER", "D7KSUPER");
+
+                
+
+                for (int i = 0; i < Convert.ToInt32(dataGridView1.RowCount); i++)
+                {
+                    string EvrakNo = dataGridView1.Rows[i].Cells[0].Value.ToString();
+
+                    if (dataGridView1.Rows[i].Cells["gridtxtbx1"].Value != null)
+                    {
+                        doc_STOK00.Load_Voucher_ForUpdate_ForceUnlock(EvrakNo);
+                        var RS_STOK0P = doc_STOK00.GetRowSetObject("STOK0P");
+
+                        RS_STOK0P.AddRow();
+                        var cnt = RS_STOK0P.GetRowCount();
+                        RS_STOK0P.SetCurrentRow(cnt);
+
+                        RS_STOK0P.EVRAKNO = EvrakNo;
+                        RS_STOK0P.FULLFILENAME1 = dataGridView1.Rows[i].Cells["gridtxtbx1"].Value.ToString();
+
+                        var sonuc = doc_STOK00.Save_Voucher();
+                        doc_STOK00.LoadEmpty_Voucher();
+                    }
+                    if (dataGridView1.Rows[i].Cells["gridtxtbx2"].Value != null)
+                    {
+                        doc_STOK00.Load_Voucher_ForUpdate_ForceUnlock(EvrakNo);
+                        var RS_STOK0P = doc_STOK00.GetRowSetObject("STOK0P");
+
+                        RS_STOK0P.AddRow();
+                        var cnt = RS_STOK0P.GetRowCount();
+                        RS_STOK0P.SetCurrentRow(cnt);
+
+                        RS_STOK0P.EVRAKNO = EvrakNo;
+                        RS_STOK0P.FULLFILENAME1 = dataGridView1.Rows[i].Cells["gridtxtbx2"].Value.ToString();
+
+                        var sonuc = doc_STOK00.Save_Voucher();
+                        doc_STOK00.LoadEmpty_Voucher();
+                    }
+                    if (dataGridView1.Rows[i].Cells["gridtxtbx3"].Value != null)
+                    {
+                        doc_STOK00.Load_Voucher_ForUpdate_ForceUnlock(EvrakNo);
+                        var RS_STOK0P = doc_STOK00.GetRowSetObject("STOK0P");
+
+                        RS_STOK0P.AddRow();
+                        var cnt = RS_STOK0P.GetRowCount();
+                        RS_STOK0P.SetCurrentRow(cnt);
+
+                        RS_STOK0P.EVRAKNO = EvrakNo;
+                        RS_STOK0P.FULLFILENAME1 = dataGridView1.Rows[i].Cells["gridtxtbx3"].Value.ToString();
+
+                        var sonuc = doc_STOK00.Save_Voucher();
+                        doc_STOK00.LoadEmpty_Voucher();
+                    }
+                }
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
